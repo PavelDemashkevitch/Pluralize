666634d35b0d3c9172d274934ef98057dd522bba
paveldemashkevitch <paveldemashkevitch@gmail.com>
2022-11-07 12:29:27 +0300

    Solution

--- PluralizeTask.cs
+++ PluralizeTask.cs
@@ -2,10 +2,23 @@
 {
 	public static class PluralizeTask
 	{
-		public static string PluralizeRubles(int count)
-		{
-			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-			return "руб.";
-		}
-	}
+        public static string PluralizeRubles(int count)
+        {
+            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+            var lastOneDigit = count % 10;
+            var lastTwoDigits = count % 100;
+            if ((lastTwoDigits > 20 || lastTwoDigits <= 10) && (lastOneDigit > 1 && lastOneDigit <= 4))
+            {
+                return "рубля";
+            }
+            else if ((lastTwoDigits > 20 || lastTwoDigits <= 10) && lastOneDigit == 1)
+            {
+                return "рубль";
+            }
+            else
+            {
+                return "рублей";
+            }
+        }
+  	}
 }
